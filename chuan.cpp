e9e1cc5f1b86de908b390257700fb6756a987d39
Timor88 <1471218388@qq.com>
2021-05-21 22:15:33 +0800

    函数架构

---
+++ chuan.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#define MAXSIZE 100
+#define ERROR 0
+#define F 0
+// #define OVERFLOW 0
+#define OK 1
+typedef char ElemType;
+using namespace std;
+int num = 0;
+
+// 串
+typedef struct HString //顶指针 + 底指针 + 大小
+{
+    
+}S;
+
+// 初始化
+int InitS(stack &s)
+{
+}
+// 串赋值
+void SCopy(){}
+// 串比较
+void SCmp(){}
+// 串连接
+void SAppend(){}
+// 求子串
+void SCat(){}
+// 串检索
+void SSearch(){}
+// 输出串
+void SPrint(){}
+// 销毁串
+void SDestory(){}
+
+int main(){
+    return 0;
+}
